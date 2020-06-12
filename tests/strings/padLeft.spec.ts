f9e0654c6688341c3675ea2918f0767afc150e28
John C. Kennedy <devjckennedy@gmail.com>
2020-06-12 20:42:38 +0900

    padLeft

---
+++ tests/strings/padLeft.spec.ts
@@ -0,0 +1,16 @@
+import { padLeft } from "@/strings/padLeft"
+
+describe("padLeft", () => {
+  test("returns string if string length > given length", () => {
+    expect(padLeft("testkey", 5)).toEqual("testkey")
+  })
+
+  test("pads string left by given length", () => {
+    expect(padLeft("test", 5)).toEqual(" test")
+  })
+
+  test("pads string left by given length and char", () => {
+    expect(padLeft("test", 5, "*")).toEqual("*test")
+    expect(padLeft("test", 8, "*")).toEqual("****test")
+  })
+})
