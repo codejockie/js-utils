b53373572c017048d070a67ae0149b0ebf2f613b
John C. Kennedy <devjckennedy@gmail.com>
2020-06-12 10:21:42 +0900

    configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.eslint.json"],
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "prettier/@typescript-eslint",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+}
