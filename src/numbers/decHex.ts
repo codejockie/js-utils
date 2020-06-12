db7bb0a2fe2e5762fef4df8337790163a6b534df
John C. Kennedy <devjckennedy@gmail.com>
2020-06-12 21:58:06 +0900

    decHex

---
+++ src/numbers/decHex.ts
@@ -0,0 +1,7 @@
+/**
+ * Converts a decimal to hex.
+ * @param num Number to convert
+ */
+export function decHex(num: number): string {
+  return (num < 15.5 ? "0" : "") + Math.round(num).toString(16)
+}
