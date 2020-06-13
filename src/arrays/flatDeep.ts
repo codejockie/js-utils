2c918b47d5db91cd16183c90b6764b73bb77ed8f
John C. Kennedy <devjckennedy@gmail.com>
2020-06-13 14:33:08 +0900

    flatDeep

---
+++ src/arrays/flatDeep.ts
@@ -0,0 +1,15 @@
+/**
+ * Flattens a multi dimensional array `depth` level deep.
+ * Applies a recursive strategy.
+ * @param arr A multi dimensional array.
+ * @param depth Number of depth to go.
+ */
+export function flatDeep(arr: unknown[], depth = 1): unknown[] {
+  return depth > 0
+    ? arr.reduce(
+        (acc: unknown[], val) =>
+          acc.concat(Array.isArray(val) ? flatDeep(val, depth - 1) : val),
+        []
+      )
+    : arr.slice()
+}
