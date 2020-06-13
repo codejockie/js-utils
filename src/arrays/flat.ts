1231ba7e6b024d58ff5caed0e86e9fb2deb152cc
John C. Kennedy <devjckennedy@gmail.com>
2020-06-13 14:32:55 +0900

    flat

---
+++ src/arrays/flat.ts
@@ -0,0 +1,7 @@
+/**
+ * Flattens a multi dimensional array one level deep.
+ * @param arr A multi dimensional array.
+ */
+export function flat(arr: unknown[]): unknown[] {
+  return ([] as unknown[]).concat(...arr)
+}
