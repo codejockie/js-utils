b6922bcb87395efc344fc69f5b23598494c19041
John C. Kennedy <devjckennedy@gmail.com>
2020-06-28 15:41:53 +0900

    toNumber

---
+++ src/numbers/toNumber.ts
@@ -0,0 +1,18 @@
+import { isBlank } from "@/misc/isBlank"
+
+export function toNumber(arg: unknown): unknown {
+  if (isBlank(arg)) {
+    return arg
+  }
+
+  const number = parseFloat(String(arg))
+
+  if (
+    Number.isNaN(Number(number)) ||
+    number.toString().length !== (arg as string).toString().length
+  ) {
+    return arg
+  }
+
+  return number
+}
