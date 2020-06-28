3fd65e263a1827a938b12d4b424ca0103beca4a9
John C. Kennedy <devjckennedy@gmail.com>
2020-06-28 15:42:15 +0900

    toType

---
+++ src/misc/toType.ts
@@ -0,0 +1,26 @@
+/**
+ * Converts a value to its actual type where possible.
+ * @param string: The value to be converted.
+ */
+export function toType(arg: unknown): unknown {
+  if (typeof arg !== "string") {
+    return arg
+  }
+
+  const lower = arg.toLowerCase().trim()
+
+  switch (lower) {
+    case "null":
+      return null
+    case "undefined":
+      return undefined
+
+    default:
+      try {
+        return JSON.parse(lower) as unknown
+      } catch {
+        // Original Type
+        return arg
+      }
+  }
+}
