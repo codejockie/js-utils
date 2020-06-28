72acfd9a77f9c508a97d43f8910eaee74e460a66
John C. Kennedy <devjckennedy@gmail.com>
2020-06-28 11:03:17 +0900

    Refactor getWeek

--- src/datetime/getWeek.ts
+++ src/datetime/getWeek.ts
@@ -1,11 +1,13 @@
-const TODAY = new Date()
-
-export function getWeek(today = TODAY): number {
-  const date = today.getDate()
-  const month = today.getMonth()
-  const year = today.getFullYear()
+/**
+ * Gets the week for the given date.
+ * @param date Custom Date instance
+ */
+export function getWeek(date: Date): number {
+  const day = date.getDate()
+  const month = date.getMonth()
+  const year = date.getFullYear()
   const start = new Date(year, 0, 1).valueOf()
-  const end = new Date(year, month, date).valueOf()
+  const end = new Date(year, month, day).valueOf()
   const dayOfYear = (end - start + 86400000) / 86400000
   return Math.ceil(dayOfYear / 7)
 }
