d9258bba6929097e32819ab1570162b210e94851
John C. Kennedy <devjckennedy@gmail.com>
2020-06-28 10:46:51 +0900

    getWeekDay

---
+++ src/datetime/getWeekDay.ts
@@ -0,0 +1,11 @@
+export function getWeekDay(date: number): string {
+  return [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ][date]
+}
