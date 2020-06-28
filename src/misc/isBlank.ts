adb4b28b073ee64bd320c766a022639347406ce5
John C. Kennedy <devjckennedy@gmail.com>
2020-06-28 15:42:05 +0900

    isBlank

---
+++ src/misc/isBlank.ts
@@ -0,0 +1,11 @@
+/**
+ * Tests whether argument is blank.
+ * @param arg Can be any of `""`, `[]`, `{}` `null`, `undefined`
+ */
+export function isBlank(arg: unknown): boolean {
+  return (
+    arg == null ||
+    (typeof arg === "string" && arg.trim() === "") ||
+    (typeof arg === "object" && Object.keys(arg as Record<string, unknown>).length === 0)
+  )
+}
