7d4cfd49be98beda7660078bca6521045c2aa6c8
John C. Kennedy <devjckennedy@gmail.com>
2020-07-11 22:00:17 +0900

    diff

---
+++ src/datetime/diff.ts
@@ -0,0 +1,9 @@
+/**
+ * Gets the number of days elapsed between two dates. `to` not inclusive.
+ * @param from Start date
+ * @param to End date
+ */
+export function diff(from: Date, to: Date): number {
+  const timeDiff = Math.abs(to.getTime() - from.getTime())
+  return Math.ceil(timeDiff / (1000 * 60 * 60 * 24))
+}
