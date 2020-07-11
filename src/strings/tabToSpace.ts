fed8b82519a75f68672893c1e5a93d591c7b7e6a
John C. Kennedy <devjckennedy@gmail.com>
2020-07-11 22:04:18 +0900

    tabToSpace

---
+++ src/strings/tabToSpace.ts
@@ -0,0 +1,10 @@
+/**
+ * Converts tab to space.
+ * @param str String with tab character
+ * @param num Number of spaces to use
+ */
+export function tabToSpace(str: string, num?: number): string {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const space = new Array(Number.isNaN(Number(num)) ? 4 : num! + 1).join(" ")
+  return str.replace(/\t/g, space)
+}
