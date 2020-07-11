56bc8c3700789692cddacddd5b1f219fad83f4c1
John C. Kennedy <devjckennedy@gmail.com>
2020-07-11 21:58:23 +0900

    uuid

---
+++ src/misc/uuid.ts
@@ -0,0 +1,9 @@
+/**
+ * Generates a universally unique identifier (UUID).
+ */
+export function uuid(): string {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (char) => {
+    const rand = (Math.random() * 16) | 0
+    return (char == "x" ? rand : (rand & 0x3) | 0x8).toString(16)
+  })
+}
