7bfbb05bfb0438ffb34de6a2ce46805a640e28ec
John C. Kennedy <devjckennedy@gmail.com>
2021-04-05 11:00:19 +0100

    update han zen

--- src/strings/hanZen.ts
+++ src/strings/hanZen.ts
@@ -2,17 +2,19 @@ import { Options } from "@/types/options";
 import { fromCharCodeAdd } from "@/utils/fromCharCode";
 
 /**
+ * Replace half-width with full-width
  * 半角から全角に置き換え
  *
+ * Tildes are converted to full-width tildes
  * チルダは全角チルダに変換
  *
- * @param {String} str 変換したい文字列
- * @param {Boolean} tilde チルダ falseを指定した場合は変換なし
- * @param {Boolean} mark 記号 falseを指定した場合は変換なし
- * @param {Boolean} hanKana 半角カナ記号 falseを指定した場合は変換なし
- * @param {Boolean} space スペース falseを指定した場合は変換なし
- * @param {Boolean} alpha 英字 falseを指定した場合は変換なし
- * @param {Boolean} num 数字 falseを指定した場合は変換なし
+ * @param {String} str 変換したい文字列 string you want to convert
+ * @param {Boolean} tilde チルダ falseを指定した場合は変換なし if false, tildes are not converted
+ * @param {Boolean} mark 記号 falseを指定した場合は変換なし if false, symbols are not converted
+ * @param {Boolean} hanKana 半角カナ記号 falseを指定した場合は変換なし if false, half-width symbols are not converted
+ * @param {Boolean} space スペース falseを指定した場合は変換なし if false, space is not converted
+ * @param {Boolean} alpha 英字 falseを指定した場合は変換なし if false, alphabets are not converted
+ * @param {Boolean} num 数字 falseを指定した場合は変換なし if false, numbers are not converted
  */
 export function hanZen(str: string, options: Options): string {
   const { alpha, hanKana, mark, num, space, tilde } = options;
